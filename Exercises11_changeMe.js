2dcd067e878047417f5b8f4b04e34b89cbe825b0
ridozaen <ridozaen@gmail.com>
2018-04-15 21:49:15 +0700

    added exercises11 file

--- Exercises11_changeMe.js
+++ Exercises11_changeMe.js
@@ -1,28 +1,34 @@
 function changeMe(arr) {
     var person = {};
     var currentYear = new Date();
-    for (var i = 0; i < arr.length; i++){
-        person.firstName = arr[i][0];
-        person.lastName = arr[i][1];
-        person.gender = arr[i][2];
-        if ( arr[i][3] === '' || arr[i][3] > currentYear.getFullYear()){
-            person.age = "Invalid Birth Year";
-        }else{
-            person.age = currentYear.getFullYear() - arr[i][3];
+    if (arr.length !== 0) {
+        for (var i = 0; i < arr.length; i++) {
+            person.firstName = arr[i][0];
+            person.lastName = arr[i][1];
+            person.gender = arr[i][2];
+            if ((!arr[i][3]) || arr[i][3] > currentYear.getFullYear()) {
+                person.age = "Invalid Birth Year";
+            } else {
+                person.age = currentYear.getFullYear() - arr[i][3];
+            }
+            console.log(i + 1 + ". " + person.firstName + " " + person.lastName + ":");
+            console.log(person);
         }
-        console.log(person.firstName + " ");                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
     }
-  }
-  
-  // TEST CASES
-  changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]); // 1. Christ Evans:
-  // { firstName: 'Christ',
-  //   lastName: 'Evans',
-  //   gender: 'Male',
-  //   age: 36 }
-  // 2. Robert Downey:
-  // { firstName: 'Robert',
-  //   lastName: 'Downey',
-  //   gender: 'Male',
-  //   age: 'Invalid Birth Year' }
-  changeMe([]); // ""
+    else {
+        console.log('""');
+    }
+}
+
+// TEST CASES
+changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]); // 1. Christ Evans:
+// { firstName: 'Christ',
+//   lastName: 'Evans',
+//   gender: 'Male',
+//   age: 36 }
+// 2. Robert Downey:
+// { firstName: 'Robert',
+//   lastName: 'Downey',
+//   gender: 'Male',
+//   age: 'Invalid Birth Year' }
+changeMe([]); // ""
