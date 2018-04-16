8717e012f95dbdcb7de19c73267daf00c38c7d56
ridozaen <ridozaen@gmail.com>
2018-04-16 08:49:11 +0700

    added exercises17 - total digit

---
+++ Exercises17_totalDigit.js
@@ -0,0 +1,14 @@
+function totalDigitRekursif(angka) {
+    // you can only write your code here!
+    if (angka.toString().length === 1) {
+        return parseInt(angka.toString()[0]);
+    }
+    return parseInt(angka.toString()[0]) + totalDigitRekursif(angka.toString().slice(1));
+}
+
+// TEST CASES
+console.log(totalDigitRekursif(512)); // 8
+console.log(totalDigitRekursif(1542)); // 12
+console.log(totalDigitRekursif(5)); // 5
+console.log(totalDigitRekursif(21)); // 3
+console.log(totalDigitRekursif(11111)); // 5
