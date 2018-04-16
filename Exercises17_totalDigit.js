7dce420b784a02aa70585ee2597e890742b06936
ridozaen <ridozaen@gmail.com>
2018-04-16 08:51:42 +0700

    added exercises17 - total digit

--- Exercises17_totalDigit.js
+++ Exercises17_totalDigit.js
@@ -12,3 +12,4 @@ console.log(totalDigitRekursif(1542)); // 12
 console.log(totalDigitRekursif(5)); // 5
 console.log(totalDigitRekursif(21)); // 3
 console.log(totalDigitRekursif(11111)); // 5
+console.log(totalDigitRekursif(12345678910)); // 46
